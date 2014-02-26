50477b58a523b7b19b4e931858fd4595280e7f26
Andy Truong <andy@toila.net>
2014-02-26 11:36:05 +0700

    Support slider which only has one item.

--- misc/js/slider.js
+++ misc/js/slider.js
@@ -5,12 +5,15 @@ Drupal.behaviors.atSlider.attach = function (context, settings) {
 
     $('.entity-bean.bean-slider').each(function(){
         // Find delta, options
+        var $e = $(this);
         var delta = $(this).parents('.at-slider:eq(0)').data('delta').replace(/^slider-/, '');
         var options = settings.at_slider[delta];
-        
-        $(this)
-            .find('.field-collection-container .field-name-slider-item .field-items, .field-collection-container .field--name-slider-item .field__items')
-            .cycle(options);
+        var apply = function(query) {
+          $e.find(query).cycle(options);
+        };
+
+        apply('.field-collection-container .field-name-slider-item .field-items');
+        apply('.field-collection-container .field--name-slider-item .field__items');
     });
 
 };
