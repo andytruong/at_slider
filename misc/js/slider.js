e929aa8488181730435bf19174d5d69589a47884
quyenbui <buiquyen1995@gmail.com>
2014-03-31 11:46:51 +0700

    Fixed issue #6

--- misc/js/slider.js
+++ misc/js/slider.js
@@ -4,6 +4,9 @@
   Drupal.behaviors.atSlider.attach = function(context, settings) {
 
     $('.entity-bean.bean-slider').each(function() {
+      if ($(this).hasClass('atSliderProccessed')) {
+        return;
+      }
       // Find delta, options
       var $e = $(this);
       var delta = $(this).parents('.at-slider:eq(0)').data('delta').replace(/^slider-/, '');
@@ -19,6 +22,7 @@
 
       apply('.field-collection-container .field-name-slider-item .field-items');
       apply('.field-collection-container .field--name-slider-item .field__items');
+      $(this).addClass('atSliderProccessed');
     });
 
   };
